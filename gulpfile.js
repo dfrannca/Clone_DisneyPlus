54c3c08829eb5980b5072865bcd0db3545099cb3
Marcos D'França <marcosdfrancass@gmail.com>
2025-02-04 00:11:28 -0300

    ajustes npm run build

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,39 @@
-const gulp = require('gulp')
-const sass = require('gulp-sass')(require('sass'))
-const imagemin = require('gulp-imagemin')
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 
+// Tarefa para minificar os scripts
 function scripts() {
     return gulp.src('./src/scripts/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('./dist/js'))
+        .pipe(gulp.dest('./dist/js'));
 }
 
+// Tarefa para compilar o SASS em CSS comprimido
 function styles() {
     return gulp.src('./src/styles/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(gulp.dest('./dist/css'))
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'));
 }
 
+// Tarefa para otimizar as imagens
 function images() {
     return gulp.src('./src/images/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('./dist/images'))
+        .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styles, images, scripts)
-exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
-    gulp.watch('.src/scripts/*.js', gulp.parallel(scripts))
+// Tarefa de build que roda as outras tarefas
+function build(done) {
+    gulp.parallel(styles, images, scripts)();
+    done(); // Sinaliza a conclusão
 }
+
+// Exportando as tarefas
+exports.build = build; // Definindo a tarefa 'build'
+exports.default = gulp.parallel(styles, images, scripts); // Tarefa padrão
+exports.watch = function () {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
+};
